14546f84aec865d20124c53fb6fe5d14bf55d779
konradkjems <konrad@tapfeed.dk>
2025-01-09 11:32:55 +0100

    Bug fixes 13

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,18 @@ export default defineConfig({
     timeout: 120000
   },
   base: "/",
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        login: 'login.html',
+        register: 'register.html',
+        about: 'about.html',
+        pricing: 'pricing.html',
+        contact: 'contact.html'
+      }
+    }
+  },
   // ... resten af konfigurationen ...
 }); 
